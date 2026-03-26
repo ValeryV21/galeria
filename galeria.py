6575422f25e7083f5ee8340f01f1d0c3a658e2ad
ValeryV21 <valeryvangelov5@gmail.com>
2026-03-26 10:04:34 +0200

    Update galeria.py

--- galeria.py
+++ galeria.py
@@ -6,7 +6,7 @@ st.title("Галерия от любими животни")
 if "animals" not in st.session_state: st.session_state.animals = []
 # Добавяне
 st.header("Добави ново животно")
-name = st.text_input("Име на животното") description = st.text_area("Описание") image_url = st.text_input("URL на картинкa")
+name = st.text_input("Име на животното") description = st.text_area("Описание") image_url = st.text_input("https://search.brave.com/images?q=Jeffrey+Epstein&context=W3sic3JjIjoiaHR0cHM6Ly91cGxvYWQud2lraW1lZGlhLm9yZy93aWtpcGVkaWEvY29tbW9ucy90aHVtYi81LzU5L0plZmZyZXlfRXBzdGVpbl9tdWdfc2hvdF8lMjhjcm9wcGVkJTI5LmpwZy81MTJweC1KZWZmcmV5X0Vwc3RlaW5fbXVnX3Nob3RfJTI4Y3JvcHBlZCUyOS5qcGciLCJ0ZXh0IjoiTXVnIHNob3Qgb2YgRXBzdGVpbiwgMjAwNiwgc21pbGluZyIsInBhZ2VfdXJsIjoiaHR0cHM6Ly9lbi53aWtpcGVkaWEub3JnL3dpa2kvSmVmZnJleV9FcHN0ZWluIn1d&sig=60205c7da96a5657e920aeebff82c0f9038d39a573de3089010b701b472395ab&nonce=7134d37bebd0da31f0a1fd27ea8ce1ee&source=infoboxImg")
 if st.button("Добави"):
 if name and description and image_url:
 st.session_state.animals.append({
